87d2e91c3d6313b958412d06f8ef048853d9b011
Sum Guy <gjserq@gmail.com>
2019-02-24 16:18:25 -0700

    Final Commit

---
+++ app/data/friends.js
@@ -0,0 +1,108 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the possible friends.
+// ===============================================================================
+
+
+var friends = [
+    {
+       "name":"Albert Flores",
+       "photo":"",
+       "scores":[
+          "5",
+          "1",
+          "4",
+          "4",
+          "5",
+          "1",
+          "2",
+          "5",
+          "4",
+          "1"
+       ]
+    },
+    {
+       "name":"Natalie Embrugio",
+       "photo":"",
+       "scores":[
+          "4",
+          "2",
+          "5",
+          "1",
+          "3",
+          "2",
+          "2",
+          "1",
+          "3",
+          "2"
+       ]
+    },
+    {
+       "name":"Scanlan Shorthalt",
+       "photo":"",
+       "scores":[
+          "5",
+          "2",
+          "2",
+          "2",
+          "4",
+          "1",
+          "3",
+          "2",
+          "5",
+          "5"
+       ]
+    },
+    {
+       "name":"Grog Strongjaw",
+       "photo":"",
+       "scores":[
+          "3",
+          "3",
+          "4",
+          "2",
+          "2",
+          "1",
+          "3",
+          "2",
+          "2",
+          "3"
+       ]
+    },
+     {
+        "name":"Weord Al",
+        "photo":"",
+        "scores":[
+           "4",
+           "3",
+           "4",
+           "1",
+           "5",
+           "2",
+           "5",
+           "3",
+           "1",
+           "4"
+        ]
+     },
+     {
+        "name":"Guy Sercu",
+        "photo":"",
+        "scores":[
+           "4",
+           "4",
+           "2",
+           "3",
+           "2",
+           "2",
+           "3",
+           "2",
+           "4",
+           "5"
+        ]
+     }
+ 
+ ]
+ 
+ // Here we export the array. This makes it accessible to other files using require. 
+ module.exports = friends;
